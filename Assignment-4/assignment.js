c3bd80b7cb22b7012252c3bb4223a26ed7e56ed5
NagothuSuryaTeja <nagothuteja@gmail.com>
2025-01-18 22:08:53 +0530

    18/01/2025 New Assignment Added

---
+++ Assignment-4/assignment.js
@@ -0,0 +1,15 @@
+/*1h*/function costOfOrder(){
+    let tshirt = 10;
+    let socks = 8;
+    let dinnerPlate = 20;
+    totalCost = tshirt+socks+dinnerPlate;
+    console.log(totalCost);
+}
+/*1i*/function moneyLeft(){
+    let balance=100;
+    let lunch = 20;
+    let dinner=50;
+    let earn = 200;
+    balance = balancd+earn-lunch-dinner;
+    console.log(balance);
+}
